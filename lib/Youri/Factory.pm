5103544e6fe5f8e56b997744e3419ae13c4144d3
Guillaume Rousse <guillomovitch@gmail.com>
2009-12-20 14:21:32 +0000

    switch to MooseX::Params::Validate instead of plain Params::Validate

--- lib/Youri/Factory.pm
+++ lib/Youri/Factory.pm
@@ -11,9 +11,11 @@ This class provides factory methods for dynamically creating instances.
 
 =cut
 
-use Params::Validate qw/:all/;
-use UNIVERSAL::require;
-use Youri::Error::WrongClass;
+use Carp;
+use Moose;
+use MooseX::Params::Validate;
+use MooseX::Types::Moose qw/HashRef/;
+use Youri::Types qw/MyClassName/;
 use version; our $VERSION = qv('0.1.0');
 
 =head2 create($class, $class, %options)
@@ -24,24 +26,16 @@ implements given interface.
 =cut
 
 sub create {
-    validate_pos(@_,
-        1,
-        {
-            type => SCALAR,
-            callbacks => {
-                'class name' => sub { eval { $_[0]->require() } }
-            }
-        },
-        {
-            type => SCALAR,
-            callbacks => {
-                'class name' => sub { eval { $_[0]->require() } },
-                'subclass'   => sub { $_[0]->isa($_[1]->[1]) }
-            }
-        },
-        { type => HASHREF, optional => 1 }
+    my $self = shift;
+    my ($interface, $class, $options) = pos_validated_list(
+        \@_,
+        { isa => MyClassName },
+        { isa => MyClassName },
+        { isa => HashRef, optional => 1 },
     );
-    my ($self, $interface, $class, $options) = @_;
+
+    croak "$class is not a subclass of $interface"
+        unless $class->isa($interface);
 
     return $class->new($options ? %$options : ());
 };
@@ -54,23 +48,16 @@ fragment.
 =cut
 
 sub create_from_configuration { 
-    validate_pos(@_,
-        1,
-        {
-            type => SCALAR,
-            callbacks => {
-                'class name' => sub { eval { $_[0]->require() } }
-            }
-        },
-        {
-            type => HASHREF,
-            callbacks => {
-                'class key' => sub { exists $_[0]->{class} },
-            }
-        },
-        { type => HASHREF, optional => 1 }
+    my $self = shift;
+    my ($interface, $config, $options) = pos_validated_list(
+        \@_,
+        { isa => MyClassName },
+        { isa => HashRef     },
+        { isa => HashRef, optional => 1 },
     );
-    my ($self, $interface, $config, $options) = @_;
+
+    croak "config hashref does not have a 'class' key"
+        unless exists $config->{class};
 
     my $all_options = {
         $config->{options} ? %{$config->{options}} : (),
